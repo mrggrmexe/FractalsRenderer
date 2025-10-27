fd44744eb217be4df4bcd21da3abdcf6ea60534a
Professor <100163658+mrggrmexe@users.noreply.github.com>
2025-10-27 21:32:01 +0300

    v1.0.11
    - png image render fix, better perfomance, less data leaks

--- Mandelbrot/imagerender.cpp
+++ Mandelbrot/imagerender.cpp
@@ -1,5 +1,14 @@
 #include "imagerender.h"
+#include <QPainter>
 
+static void tuneView(QGraphicsView* v) {
+    if (!v) return;
+    v->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    v->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    v->setFrameShape(QFrame::NoFrame);
+    v->setRenderHint(QPainter::SmoothPixmapTransform, true);
+    v->setViewportUpdateMode(QGraphicsView::SmartViewportUpdate);
+}
 
 // CONSTRUCTORS /////////////////////////////////
 
@@ -8,37 +17,48 @@ ImageRender::ImageRender(Ui::MainWindow *&_ui)
 {
     graphicsView = new QGraphicsView;
     scene        = new QGraphicsScene;
+    tuneView(graphicsView);
+    graphicsView->setScene(scene);
 }
 
-ImageRender::ImageRender(uint16_t       _height,
-                         uint16_t       _width,
-                         uint16_t       _x,
-                         uint16_t       _y,
-                         QString        _PATH,
+ImageRender::ImageRender(uint16_t _height,
+                         uint16_t _width,
+                         uint16_t _x,
+                         uint16_t _y,
+                         QString  _PATH,
                          Ui::MainWindow *&_ui,
-                         QWidget        *add)
+                         QWidget *add)
     : height(_height), width(_width),
-      x(_x), y(_y),
-      PATH(_PATH),
-      ui(_ui)
+    x(_x), y(_y),
+    PATH(_PATH),
+    ui(_ui)
 {
     graphicsView = new QGraphicsView(add);
-    graphicsView->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    graphicsView->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     scene        = new QGraphicsScene(add);
+    tuneView(graphicsView);
+    graphicsView->setScene(scene);
     render();
 }
 
 ImageRender::ImageRender(QWidget *add)
 {
     graphicsView = new QGraphicsView(add);
-    graphicsView->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    graphicsView->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     scene        = new QGraphicsScene(add);
+    tuneView(graphicsView);
+    graphicsView->setScene(scene);
 }
 
-/////////////////////////////////////////////////
-
+ImageRender::~ImageRender()
+{
+    if (graphicsView && !graphicsView->parent()) {
+        delete graphicsView;
+        graphicsView = nullptr;
+    }
+    if (scene && !scene->parent()) {
+        delete scene;
+        scene = nullptr;
+    }
+}
 
 // SETTERS //////////////////////////////////////
 
@@ -59,66 +79,44 @@ void ImageRender::setPath(QString _PATH)
     PATH = _PATH;
 }
 
-/////////////////////////////////////////////////
-
-
 // GETTERS //////////////////////////////////////
 
-uint16_t ImageRender::getHeight()
-{
-    return height;
-}
-
-uint16_t ImageRender::getWidth()
-{
-    return width;
-}
-
-uint16_t ImageRender::getX()
-{
-    return x;
-}
-
-uint16_t ImageRender::getY()
-{
-    return y;
-}
-
-QString ImageRender::getPath()
-{
-    return PATH;
-}
-
-/////////////////////////////////////////////////
-
+uint16_t ImageRender::getHeight() { return height; }
+uint16_t ImageRender::getWidth()  { return width;  }
+uint16_t ImageRender::getX()      { return x;      }
+uint16_t ImageRender::getY()      { return y;      }
+QString  ImageRender::getPath()   { return PATH;   }
 
 // FUNCTIONAL ///////////////////////////////////
 
 bool ImageRender::render()
 {
+    if (height == 0 || width == 0) return false;
+
     if (!img.load(PATH)) return false;
+
     img = img.scaled(height, width,
                      Qt::IgnoreAspectRatio,
                      Qt::SmoothTransformation);
 
     QPixmap pixmap = QPixmap::fromImage(img);
+
     graphicsView->setGeometry(x, y, height, width);
     graphicsView->setStyleSheet("background: transparent");
-    graphicsView->setFrameShape(QFrame::NoFrame);
 
     if (!pixmapItem) {
         pixmapItem = scene->addPixmap(pixmap);
+        pixmapItem->setTransformationMode(Qt::SmoothTransformation);
+        pixmapItem->setCacheMode(QGraphicsItem::DeviceCoordinateCache);
+        pixmapItem->setPos(0, 0);
     } else {
         pixmapItem->setPixmap(pixmap);
     }
-
+    scene->setSceneRect(pixmapItem->boundingRect());
     graphicsView->setScene(scene);
     graphicsView->show();
     return true;
 }
-
-
-
 
 bool ImageRender::render(uint16_t _height, uint16_t _width)
 {
@@ -148,7 +146,3 @@ bool ImageRender::render(uint16_t _height, uint16_t _width,
     PATH   = _PATH;
     return this->render();
 }
-
-/////////////////////////////////////////////////
-
-
--- Mandelbrot/imagerender.h
+++ Mandelbrot/imagerender.h
@@ -11,52 +11,38 @@
 #include <QPixmap>
 #include <QFrame>
 
-
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 class ImageRender
 {
-
 public:
-
     // CONSTRUCTORS ////////////////
-
-    ImageRender(Ui::MainWindow    *&_ui);
-    ImageRender(QWidget           *add);
-    ImageRender(uint16_t          _height,
-                uint16_t          _width,
-                uint16_t          _x,
-                uint16_t          _y,
-                QString           _PATH,
-                Ui::MainWindow    *&_ui,
-                QWidget           *add);
-
-    ////////////////////////////////
-
+    ImageRender(Ui::MainWindow *&_ui);
+    ImageRender(QWidget *add);
+    ImageRender(uint16_t _height,
+                uint16_t _width,
+                uint16_t _x,
+                uint16_t _y,
+                QString  _PATH,
+                Ui::MainWindow *&_ui,
+                QWidget *add);
+    ~ImageRender();
 
     // SETTERS /////////////////////
-
     void setSize(uint16_t _height, uint16_t _width);
     void setPos (uint16_t _x,      uint16_t _y);
     void setPath(QString  _PATH);
 
-    ////////////////////////////////
-
     // GETTERS /////////////////////
-
     uint16_t getHeight();
     uint16_t getWidth();
     uint16_t getX();
     uint16_t getY();
     QString  getPath();
 
-    ////////////////////////////////
-
     // FUNCTIONAL //////////////////
-
-    // RENDER OVERLOAD
     bool render();
     bool render(uint16_t _height, uint16_t _width);
     bool render(uint16_t _height, uint16_t _width,
@@ -65,20 +51,15 @@ public:
                 uint16_t _x,      uint16_t _y,
                 QString  _PATH);
 
-    ////////////////////////////////
-
-private slots:
-
 private:
-    uint16_t       height, width;
-    uint16_t       x, y;
+    uint16_t       height = 0, width = 0;
+    uint16_t       x = 0, y = 0;
     QString        PATH;
     QGraphicsView  *graphicsView = nullptr;
     QGraphicsScene *scene = nullptr;
     QImage         img;
-    Ui::MainWindow *ui;
+    Ui::MainWindow *ui = nullptr;
     QGraphicsPixmapItem* pixmapItem = nullptr;
 };
 
 #endif // IMAGERENDER_H
-
